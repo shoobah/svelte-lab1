ff67c3551895a1df9b642f61c205f6830adc689a
Anders Sjöberg <anders.sjoberg@teleopti.com>
2019-04-24 16:06:48 +0200

    Testing 🤟

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,28 @@
 <script>
+	import { fade, fly } from "svelte/transition";
 	export let name;
 
 	let list = [];
 
-	const getNumber = (min, max) => {
-	  return Math.floor(Math.random() * max + min);
+	const getItem = (min, max) => {
+	  return { id: Math.random(), num: Math.floor(Math.random() * max + min) };
 	};
 
-	const handleClick = () => {
-	  list = [...list, getNumber(0, 100)].sort((a, b) =>
-	    a < b ? -1 : a > b ? 1 : 0
-	  );
+	const addItem = () => {
+	  list = [...list, getItem(0, 100)];
+	};
+
+	const add100Items = () => {
+	  for (let n = 0; n < 10000; n++) {
+	    list = [...list, getItem(0, 100)];
+	    // .sort((a, b) =>
+	    //   a < b ? -1 : a > b ? 1 : 0
+	    // );
+	  }
+	};
+
+	const clearList = () => {
+	  list = [];
 	};
 </script>
 
@@ -29,13 +41,15 @@
 </style>
 
 <h1>Hello {name}!</h1>
-<button on:click={handleClick}>Klicka</button>
+<button on:click={addItem}>Lägg till</button>
+<button on:click={add100Items}>Lägg till 100</button>
+<button on:click={clearList}>Rensa</button>
 <div>
 <span>Length={list.length}</span>
 	<ul>
-		{#each list as item (item)}
+		{#each list as item (item.id)}
 			<li>
-				{item}
+				{item.num}
 			</li>
 			{:else}
 			<li>Tom</li>
