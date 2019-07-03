daf4c5653f582c6ecf331314783d7a414bc3a561
shoobah <shoobah@gmail.com>
2019-07-03 02:11:53 +0200

    Fixed reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,64 @@
 <script>
-	import { fade, fly } from "svelte/transition";
-	import Checkbox from "./components/Checkbox.svelte";
-	import Month from "./components/calendar/Month.svelte";
+  import { fade, fly } from "svelte/transition";
+  import Checkbox from "./components/Checkbox.svelte";
+  import Month from "./components/calendar/Month.svelte";
 
-	export let name;
+  export let name;
 
-	let list = [];
-	let listVisible = false;
+  let list = [];
+  let listVisible = false;
 
-	const getItem = (min, max) => {
-	  return { id: Math.random(), num: Math.floor(Math.random() * max + min) };
-	};
+  const getItem = (min, max) => {
+    return { id: Math.random(), num: Math.floor(Math.random() * max + min) };
+  };
 
-	const addItem = () => {
-	  list = [...list, getItem(0, 100)];
-	};
+  const addItem = () => {
+    list = [...list, getItem(0, 100)];
+  };
 
-	const add100Items = () => {
-	  for (let n = 0; n < 100; n++) {
-	    list = [...list, getItem(0, 100)];
-	  }
-	};
+  const add100Items = () => {
+    for (let n = 0; n < 100; n++) {
+      list = [...list, getItem(0, 100)];
+    }
+  };
 
-	const clearList = () => {
-	  list = [];
-	};
+  const clearList = () => {
+    list = [];
+  };
 
-	const handleToggle = event => {
-	  listVisible = event.detail;
-	};
+  const handleToggle = event => {
+    listVisible = event.detail;
+  };
 </script>
 
 <style>
-	h1 {
-	  color: rgb(32, 12, 87);
-	}
-	ul {
-	  list-style: none;
-	}
+  h1 {
+    color: rgb(32, 12, 87);
+  }
+  ul {
+    list-style: none;
+  }
 
-	li {
-	  font-size: 20px;
-	  cursor: pointer;
-	}
+  li {
+    font-size: 20px;
+    cursor: pointer;
+  }
 
-	#myList,
-	#myTempText {
-	  position: absolute;
-	}
+  #myList,
+  #myTempText {
+    position: absolute;
+  }
 
-	#app {
-	  position: relative;
-	}
+  #app {
+    position: relative;
+  }
 </style>
 
 <div id="app">
-	<h1>Hello {name}!</h1>
-	<Month />
-	<!-- <input type="checkbox" on:change={handleToggle}>Show list<br/> -->
-	<Checkbox on:checked={handleToggle} />
+  <h1>Hello {name}!</h1>
+  <Month />
+  <!-- <input type="checkbox" on:change={handleToggle}>Show list<br/> -->
+  <!-- <Checkbox on:checked={handleToggle} />
 	{#if listVisible===true}
 		<div transition:fly="{{y:200, duration:500}}" id="myList">
 			<h2>A list</h2>
@@ -78,5 +78,5 @@
 		</div>
 		{:else}
 		<h2 id="myTempText" transition:fly="{{y:200, duration:500}}" >Check the chekcbox to see a list</h2>
-	{/if}
+	{/if} -->
 </div>
--- src/components/calendar/Month.svelte
+++ src/components/calendar/Month.svelte
@@ -1,88 +1,97 @@
 <script>
-	import { DateTime } from "luxon";
-	const daysWidth = 7;
-	const hoursHeight = 4 * 24;
-	let width = 1000;
-	let height = 1000;
-	let xRatio = width / daysWidth;
-	let yRatio = height / hoursHeight;
+  import { DateTime } from "luxon";
+  const daysWidth = 7;
+  const hoursHeight = 4 * 24;
+  let width = 1000;
+  let height = 1000;
+  let xRatio = width / daysWidth;
+  let yRatio = height / hoursHeight;
+  let test = {};
 
-	$: height = width;
+  $: height = width;
 
-	$: xRatio = width / daysWidth;
-	$: yRatio = height / hoursHeight;
+  $: xRatio = width / daysWidth;
+  $: yRatio = height / hoursHeight;
 
-	$: getLines = () => {
-	  let lines = [];
-	  for (let x = 0; x < daysWidth; x += 1) {
-	    for (let y = 0; y < hoursHeight; y += 24) {
-	      lines.push({ x: x * xRatio, y: y * yRatio });
-	    }
-	  }
-	  return lines;
-	};
+  $: getLines = () => {
+    let lines = [];
+    for (let x = 0; x < daysWidth; x += 1) {
+      for (let y = 0; y < hoursHeight; y += 24) {
+        lines.push({ x: x * xRatio, y: y * yRatio });
+      }
+    }
+    return lines;
+  };
 
-	const getTimeRect = (dtFrom, dtTo) => {
-	  console.log("dtFrom, dtTo", { from: dtFrom.toISO(), to: dtTo.toISO() });
-	  console.log("dtFrom.day / daysWidth", Math.floor(dtFrom.day / daysWidth));
-	  console.log('dtTo.diff(dtFrom, "days")', dtTo.diff(dtFrom, "days").days);
-	  console.log('dtTo.diff(dtFrom, "hours")', dtTo.diff(dtFrom, "hours").hours);
-	  console.log("xRatio", xRatio);
-	  console.log("yRatio", yRatio);
-	  return {
-	    x: dtFrom.weekday * xRatio,
-	    width: Math.ceil(dtTo.diff(dtFrom, "days").days) * xRatio,
-	    y: (Math.floor(dtFrom.day / daysWidth) * 24 + dtFrom.hour) * yRatio,
+  $: getTimeRect = (dtFrom, dtTo) => {
+    return {
+      x: dtFrom.weekday * xRatio,
+      width: Math.ceil(dtTo.diff(dtFrom, "days").days) * xRatio,
+      y: (Math.floor(dtFrom.day / daysWidth) * 24 + dtFrom.hour) * yRatio,
 
-	    height: dtTo.diff(dtFrom, "hours").hours * yRatio
-	  };
-	};
+      height: dtTo.diff(dtFrom, "hours").hours * yRatio
+    };
+  };
 
-	const from = DateTime.fromISO("2019-04-02T08:17:18.788+02:00", {
-	  locale: "sv"
-	});
-	const to = from.plus({ hours: 6 });
+  const from = DateTime.fromISO("2019-04-02T08:17:18.788+02:00", {
+    locale: "sv"
+  });
+  const to = from.plus({ hours: 6 });
 
-	const test = {
-	  from,
-	  to,
-	  rect: getTimeRect(from, to)
-	};
-	console.log("test", test);
+  $: test = {
+    from,
+    to,
+    rect: getTimeRect(from, to)
+  };
 
-	const handleDayClick = info => {
-	  console.log(
-	    `A meeting starting at ${from.toLocaleString()} lasting ${
-	      to.diff(from, "minutes").minutes
-	    } minutes`
-	  );
-	};
+  const handleDayClick = info => {
+    console.log(
+      `A meeting starting at ${from.toLocaleString()} lasting ${
+        to.diff(from, "minutes").minutes
+      } minutes`
+    );
+  };
 </script>
 
 <style>
-	.background {
-	  fill: white;
-	  stroke: black;
-	}
+  .background {
+    fill: white;
+    stroke: black;
+  }
 
-	.day {
-	  fill: lime;
-	  stroke: none;
-	  cursor: pointer;
-	}
+  .day {
+    fill: lime;
+    stroke: none;
+    cursor: pointer;
+  }
 </style>
 
 <div id="month-wrapper">
-	<div>
-	{DateTime.local()}<br/>
-	<input type=range bind:value={width} min=25 max=1000 />
-	</div>
-	<svg height="80vh" width="80vh" viewBox="0 0 1000 1000" preserveAspectRatio xmlns="http://www.w3.org/2000/svg">
-		<rect x="0" y="0" width={width} height={width} class="background" />
-		{#each getLines() as line}
-			<line x1="0" y1={line.y} x2={width} y2={line.y} stroke="black" />
-			<line x1={line.x} y1=0 x2={line.x} y2={height} stroke="black" />
-		{/each}
-		<rect class="day" x={test.rect.x} y={test.rect.y} width={test.rect.width} height={test.rect.height} on:click={()=>{handleDayClick(test)}} />
-	</svg>
+  <div>
+     {DateTime.local()}
+    <br />
+    <input type="range" bind:value={width} min="25" max="1000" />
+    <pre>{test.rect.x}</pre>
+  </div>
+  <svg
+    height="80vh"
+    width="80vh"
+    viewBox="0 0 1000 1000"
+    preserveAspectRatio
+    xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" {width} height={width} class="background" />
+    {#each getLines() as line}
+      <line x1="0" y1={line.y} x2={width} y2={line.y} stroke="black" />
+      <line x1={line.x} y1="0" x2={line.x} y2={height} stroke="black" />
+    {/each}
+    <rect
+      class="day"
+      x={test.rect.x}
+      y={test.rect.y}
+      width={test.rect.width}
+      height={test.rect.height}
+      on:click={() => {
+        handleDayClick(test);
+      }} />
+  </svg>
 </div>
